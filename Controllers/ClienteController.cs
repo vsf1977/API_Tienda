0c88e14380390a46cef5bef4725723a566c037fe
WINDOWS 10 <WINDOWS 10@DESKTOP-2TQNHR1>
2021-07-17 13:16:16 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante_API.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+
+namespace Restaurante_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public List<Cliente> Get()
+        {
+            Cliente Cliente = new Cliente();
+            return Cliente.list();
+        }
+
+        [HttpGet("/api/cliente/{id}")]
+        public Cliente Get(string id)
+        {
+            Cliente Cliente = new Cliente();
+            return Cliente.GetCliente(id);
+        }
+        [HttpPost("/api/cliente")]
+        public HttpResponseMessage Post([FromBody] Cliente data)
+        {
+            Cliente Cliente = new Cliente();
+            Cliente.IdCliente = data.IdCliente;
+            Cliente.nombre = data.nombre;
+            Cliente.apellidos = data.apellidos;
+            Cliente.direccion = data.direccion;
+            Cliente.telefono = data.telefono;
+            try
+            {
+                Cliente.insertar();
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch (SqlException ex)
+            {
+                HttpResponseMessage resp = new HttpResponseMessage();
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                string v = ex.Message.Trim();
+                int index = v.IndexOf("\r");
+                v = v.Substring(0, index - 1);
+                resp.ReasonPhrase = v;
+                return resp;
+            }
+        }
+        [HttpPut("/api/cliente")]
+        public HttpResponseMessage Put([FromBody] Cliente data)
+        {
+            Cliente Cliente = new Cliente();
+            Cliente.IdCliente = data.IdCliente;
+            Cliente.nombre = data.nombre;
+            Cliente.apellidos = data.apellidos;
+            Cliente.direccion = data.direccion;
+            Cliente.telefono = data.telefono;
+            try
+            {
+                Cliente.actualizar();
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch (SqlException ex)
+            {
+                HttpResponseMessage resp = new HttpResponseMessage();
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                string v = ex.Message.Trim();
+                int index = v.IndexOf("\r");
+                v = v.Substring(0, index - 1);
+                resp.ReasonPhrase = v;
+                return resp;
+            }
+        }
+
+        [HttpDelete("/api/cliente/{id}")]
+        public HttpResponseMessage Delete(string id)
+        {
+            Cliente Cliente = new Cliente();
+            Cliente.IdCliente = id;           
+            try
+            {
+                Cliente.borrar();
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (SqlException ex)
+            {
+                HttpResponseMessage resp = new HttpResponseMessage();
+                resp.StatusCode = HttpStatusCode.InternalServerError;
+                string v = ex.Message.Trim();
+                int index = v.IndexOf("\r");
+                v = v.Substring(0, index - 1);
+                resp.ReasonPhrase = v;
+                return resp;
+            }
+        }
+        
+
+
+    }
+                
+}
---
+++ DataAccess/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Tienda;
+using Microsoft.Extensions.Configuration;
+
+namespace API_Tienda.DataAccess
+{
+    public class Database
+    {
+        SqlConnection databaseConnection;
+        public SqlConnection Conectar()
+        {
+            string cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["DefaultConnection"];
+            databaseConnection = new SqlConnection(cadena);
+            if (databaseConnection.State == System.Data.ConnectionState.Closed)
+                databaseConnection.Open();
+            return databaseConnection;
+        }
+
+        public void Desconectar()
+        {
+            if (databaseConnection.State == System.Data.ConnectionState.Open)
+                databaseConnection.Close();
+        }
+    }
+}
