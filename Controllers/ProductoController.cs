4058d6b202d732b62215cb485ae6019623cccef4
Vladimir Silva <vsf1977@gmail.com>
2021-07-17 13:37:05 -0500

    Configuracion inicial clase producto

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Tienda.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductoController : Controller
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using API_Tienda.DataAccess;
+using System.Data.SqlClient;
 
 namespace API_Tienda.Models
 {
     public class Producto
     {
+        SqlCommand comando;
+        SqlDataReader cursor;
+        Database database = new Database();
+        public int idProducto { get; set; }
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+        public byte descuento { get; set; }
+        public string pais { get; set; }
+
     }
 }
