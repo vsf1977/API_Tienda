2de507c2e933789f52b57bae5de9ccc1f9c266f6
Vladimir Silva <vsf1977@gmail.com>
2021-07-18 00:02:25 -0500

    Metodos getAll, Consultas por parametros y Metodo para guardar

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_Tienda.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace API_Tienda.Controllers
 {
@@ -6,10 +8,60 @@ namespace API_Tienda.Controllers
     [Route("api/v1/[controller]")]
     public class ProductoController : Controller
     {
-        [HttpGet]
-        public int Get()
+        [HttpGet("/api/v1/producto/search")]
+        public ActionResult Get(string? nombre, decimal? preciomax, decimal? preciomin, byte? descuento, string? pais)
         {
-            return 1;
+            string query_string = "";
+            if (!string.IsNullOrEmpty(nombre))
+                query_string = query_string + " UPPER(nombre) like CONCAT('%', UPPER(@nombre), '%')";
+            if (preciomax.HasValue && preciomin.HasValue && preciomax >= preciomin)
+                query_string = query_string.Length > 0 ? query_string + " and precio <= @preciomax and precio >= @preciomin" : query_string + " precio <= @preciomax and precio >= @preciomin";
+            if (descuento.HasValue)
+                query_string = query_string.Length > 0 ? query_string + " and descuento = @descuento" : query_string + " descuento = @descuento";
+            if (!string.IsNullOrEmpty(pais))
+                query_string = query_string.Length > 0 ? query_string + " and UPPER(pais) like CONCAT('%', UPPER(@pais), '%')" : query_string + " UPPER(pais) like CONCAT('%', UPPER(@pais), '%')";
+            query_string = query_string.Length > 0 ? "select * from producto where" + query_string : "";
+            if (query_string.Length > 0)
+            {
+                Producto producto = new Producto();
+                return Ok(producto.list(nombre, preciomax, preciomin, descuento, pais, query_string));
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            Producto producto = new Producto();
+            if (producto.GetAll().Count > 0)
+                return Ok(producto.GetAll());
+            else
+                return NotFound();
+        }
+
+        [HttpPost("/api/v1/producto")]
+        public ActionResult Post([FromForm] Producto data)
+        {
+            Producto producto = new Producto();
+            producto.nombre = data.nombre;
+            producto.descripcion = data.descripcion;
+            producto.precio = data.precio;
+            producto.descuento = data.descuento;
+            producto.pais = data.pais;
+            try
+            {
+                producto.insertar();
+                return Ok();
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(error: ex.ErrorCode);
+            }
+        }
+
+
     }
 }
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,4 +1,5 @@
 ﻿using API_Tienda.DataAccess;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace API_Tienda.Models
@@ -10,9 +11,118 @@ namespace API_Tienda.Models
         Database database = new Database();
         public int idProducto { get; set; }
         public string nombre { get; set; }
+        public string descripcion { get; set; }
         public decimal precio { get; set; }
         public byte descuento { get; set; }
         public string pais { get; set; }
+
+        public List<Producto> GetAll()
+        {
+            comando = new SqlCommand("select * from producto", database.Conectar());
+            cursor = comando.ExecuteReader();
+            List<Producto> productos = new List<Producto>();
+            if (cursor.HasRows)
+            {
+                while (cursor.Read())
+                {
+                    Producto producto = new Producto();
+                    producto.idProducto = cursor.GetInt32(0);
+                    producto.nombre = cursor.GetString(1);
+                    producto.descripcion = cursor.GetString(2);
+                    producto.precio = cursor.GetDecimal(3);
+                    producto.descuento = cursor.GetByte(4);
+                    producto.pais = cursor.GetString(5);
+                    productos.Add(producto);
+                }
+            }
+            database.Desconectar();
+            return productos;
+        }
+
+        public List<Producto> list(string? nombre, decimal? preciomax, decimal? preciomin, byte? descuento, string? pais, string query_string)
+        {
+            comando = new SqlCommand(query_string, database.Conectar());
+            if (query_string.IndexOf("nombre") >= 0)
+            {
+                SqlParameter nombreParam = new SqlParameter("@nombre", System.Data.SqlDbType.VarChar, 50);
+                nombreParam.Value = nombre;
+                comando.Parameters.Add(nombreParam);
+            }
+            if (query_string.IndexOf("pais") >= 0)
+            {
+                SqlParameter paisParam = new SqlParameter("@pais", System.Data.SqlDbType.VarChar, 100);
+                paisParam.Value = pais;
+                comando.Parameters.Add(paisParam);
+            }
+            if (query_string.IndexOf("precio") >= 0)
+            {
+                SqlParameter preciomaxParam = new SqlParameter("@preciomax", System.Data.SqlDbType.Decimal);
+                preciomaxParam.Value = preciomax;
+                preciomaxParam.Precision = 10;
+                preciomaxParam.Scale = 3;
+                comando.Parameters.Add(preciomaxParam);
+                SqlParameter preciominParam = new SqlParameter("@preciomin", System.Data.SqlDbType.Decimal);
+                preciominParam.Value = preciomin;
+                preciominParam.Precision = 10;
+                preciominParam.Scale = 3;
+                comando.Parameters.Add(preciominParam);
+            }
+            if (query_string.IndexOf("descuento") >= 0)
+            {
+                SqlParameter descuentoParam = new SqlParameter("@descuento", System.Data.SqlDbType.TinyInt);
+                descuentoParam.Value = descuento;
+                comando.Parameters.Add(descuentoParam);
+            }
+            comando.Prepare();
+            cursor = comando.ExecuteReader();
+            List<Producto> productos = new List<Producto>();
+            if (cursor.HasRows)
+            {
+                while (cursor.Read())
+                {
+                    Producto producto = new Producto();
+                    producto.idProducto = cursor.GetInt32(0);
+                    producto.nombre = cursor.GetString(1);
+                    producto.descripcion = cursor.GetString(2);
+                    producto.precio = cursor.GetDecimal(3);
+                    producto.descuento = cursor.GetByte(4);
+                    producto.pais = cursor.GetString(5);
+                    productos.Add(producto);
+                }
+            }
+            database.Desconectar();
+            return productos;
+        }
+
+        public void insertar()
+        {
+            comando = new SqlCommand(null, database.Conectar());
+            comando.CommandText = "insert into producto values (@nombre, @desc, @precio, @descuento, @pais)";
+
+            SqlParameter nombreParam = new SqlParameter("@nombre", System.Data.SqlDbType.VarChar, 50);
+            SqlParameter descParam = new SqlParameter("@desc", System.Data.SqlDbType.VarChar, 100);
+            SqlParameter precioParam = new SqlParameter("@precio", System.Data.SqlDbType.Decimal);
+            SqlParameter descuentoParam = new SqlParameter("@descuento", System.Data.SqlDbType.TinyInt);
+            SqlParameter paisParam = new SqlParameter("@pais", System.Data.SqlDbType.VarChar, 50);
+
+            nombreParam.Value = nombre;
+            descParam.Value = descripcion;
+            precioParam.Value = precio;
+            precioParam.Precision = 10;
+            precioParam.Scale = 3;
+            descuentoParam.Value = descuento;
+            paisParam.Value = pais;
+
+            comando.Parameters.Add(nombreParam);
+            comando.Parameters.Add(descParam);
+            comando.Parameters.Add(precioParam);
+            comando.Parameters.Add(descuentoParam);
+            comando.Parameters.Add(paisParam);
+
+            comando.Prepare();
+            comando.ExecuteNonQuery();
+            database.Desconectar();
+        }
 
     }
 }
